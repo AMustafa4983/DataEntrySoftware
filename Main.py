bf6a58c0b8401ef687c9bb28fb4fe295b9687e01
Abdelrahman Mustafa <96976680+Abdelrahman4983@users.noreply.github.com>
2023-08-12 20:52:27 +0300

    AddRecords Class added and main.py file modified.

--- Main.py
+++ Main.py
@@ -1,3 +1,14 @@
-from gui import SelectReviewers, RevieresData, Exit
-from utils import AddRecords
+from gui.SelectReviewers import SelectReviewers
+from gui.ReviewersData import ReviewersData
+from gui.Exit import Exit
+from utils.AddRecords import AddRecords
 
+wordObj = AddRecords('template/template.xlsx')
+wordObj.loadTemp()
+
+
+records2 = {'A':[0,0,1,2,3,1,4,2,0,1,4,3,2],"S S":[0,0,1,2,3,"Abs",4,2,0,'Abs',4,3,2], "SH":[2,3,4,2,3,1,4,2,0,1,4,3,2]}
+
+wordObj.addReviewerData(records2)
+
+wordObj.saveExcel('test')
--- utils/AddRecords.py
+++ utils/AddRecords.py
@@ -1,4 +1,40 @@
+import openpyxl
+
+columnsName = {"A":6, "S":7, "SH":8, "AM":9, "AS":10, "H":11,"J":12, "S S":13, "H L":14, "M":15, "MA":16}
+
 class AddRecords():
     '''
         this class used to add values to excel sheet.
+        loadTemp() - Read Template and retrun active file.
     '''
+
+    def __init__(self, path):
+        self.path = path
+        
+        return None
+    
+    def loadTemp(self):
+        try:
+            self.wordbook_obj = openpyxl.load_workbook(self.path)
+            print("Wordbook loaded!")
+            self.sheet_obj = self.wordbook_obj.active
+            print(f"Sheet {self.sheet_obj.title} is active now!")
+        except Exception:
+            raise "Loading template goes wrong | exception:"+ Exception
+
+    def saveExcel(self,name):
+        try:
+            self.wordbook_obj.save(f'output/{name}.xlsx')
+            return f"File Saved To output/{name}.xlsx"
+        except Exception:
+            raise "Saving File goes wrong | exception:"+ Exception
+
+    def addReviewerData(self,scores):
+        try:
+            for key in scores.keys():
+                column = columnsName[key]
+                values = scores[key]
+                for i in range(len(values)):
+                    self.sheet_obj.cell(row=i+2,column=column,value=values[i])
+        except Exception:
+            raise "Adding Data goes wrong | exception:"+ Exception
